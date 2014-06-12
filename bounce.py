456187423724c748846b774bdb4e922bd6967b4d
EckoZero <lentonp@gmail.com>
2014-06-12 16:46:43 +0100

    Updated to add Game Over screen

--- bounce.py
+++ bounce.py
@@ -37,57 +37,84 @@ x_change = 10
 rect_top = random.randint(40, 660)
 rect_bottom = rect_top - 30
 
-
-
-
 #Two different sounds - a ding for if you hit space bar at correct
 #time, a buzz for incorrect timing
 ding = pygame.mixer.Sound('ding.wav')
 buzz = pygame.mixer.Sound('buzz.wav')
 
-while True:
-	window.fill(BLACK)
-	pygame.draw.rect(window, WHITE, (10, x, 360, 5))
-	pygame.draw.rect(window, RED, (10, rect_top, 360, 5))
-	pygame.draw.rect(window, RED, (10, rect_bottom, 360, 5))
+playing = True
+def playGame(playing, score):
+	global bounce, x, x_change, rect_top, rect_bottom
+	while playing:
+		window.fill(BLACK)
+		pygame.draw.rect(window, WHITE, (10, x, 360, 5))
+		pygame.draw.rect(window, RED, (10, rect_top, 360, 5))
+		pygame.draw.rect(window, RED, (10, rect_bottom, 360, 5))
 
 
 	
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
 		
-		if event.type == KEYDOWN:
-			if event.key == K_SPACE:
-				if (x - 5) <= rect_top and (x + 5) >= rect_bottom:
-					ding.play()
-					score +=1
-					rect_top = random.randint(40, 660)
-					rect_bottom = rect_top - 30
-					bounce = 0
-				else:
-					buzz.play()
-					score = 0
-					bounce = 0
-				
-	if x > 650:
-		x_change *= -1
-		bounce += 1
+			if event.type == KEYDOWN:
+				if event.key == K_SPACE:
+					if (x - 5) <= rect_top and (x + 5) >= rect_bottom:
+						ding.play()
+						score +=1
+						rect_top = random.randint(40, 660)
+						rect_bottom = rect_top - 30
+						bounce = 0
+					else:
+						buzz.play()
+						bounce = 0
+						playing = False
+			
+		if x > 650:
+			x_change *= -1
+			bounce += 1
 
-	elif x < 1:
-		x_change *= -1
-		bounce += 1
+		elif x < 1:
+			x_change *= -1
+			bounce += 1
 
-	if bounce > 1:
-		buzz.play()
-		score = 0
+		if bounce > 1 and score > 0:
+			buzz.play()
+			bounce = 0
+			playing = False
 	
 	
-	x += x_change
+		x += x_change
 		
 	# Scores and mandatory updates
-	text = font.render("Score: " + str(score), True, WHITE)
-	window.blit(text, (10, 660))
+		text = font.render("Score: " + str(score), True, WHITE)
+		window.blit(text, (10, 660))
+		pygame.display.update()
+		fpsClock.tick(30)
+	gameOver(playing, score)
+	
+def gameOver(playing, score):
+	window.fill(WHITE)
+	gOFont = pygame.font.Font('freesansbold.ttf', 50)
+	gameOverText = gOFont.render("Game Over!", True, BLACK)
+	displayScore = gOFont.render("You scored " + str(score), True, BLACK)
+	queryText = font.render("Play again? Y/N", True, RED)
+	window.blit(gameOverText, (30, 10))
+	window.blit(displayScore, (30, 100))
+	window.blit(queryText, (30, 300))
+
 	pygame.display.update()
 	fpsClock.tick(30)
+	while playing == False:
+		for event in pygame.event.get():
+			if event.type == KEYDOWN:
+				if event.key == K_y:
+					score = 0
+					playing = True
+					playGame(playing, score)
+				elif event.key == K_n:
+					pygame.quit()
+					sys.exit()
+					
+playGame(playing, score)
